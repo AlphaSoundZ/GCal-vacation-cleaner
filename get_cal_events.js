3de2a35f2edb80eb27062eed3c1c4fb07a164a2b
AlphaSoundZ <jan.jacob.holst@gmail.com>
2023-03-05 12:59:09 +0100

    ✨ feat: delete all events in time range

--- get_cal_events.js
+++ get_cal_events.js
@@ -19,43 +19,93 @@ const calendar = google.calendar({
 // Import luxon for date and time handling
 const { DateTime } = require('luxon');
 
+// ###############################
+main();
+
 // Get timezone from Google Calendar settings
-calendar.settings.get({
-    setting: 'timezone'
-}, async (err, res) => {
-    if (err) return console.error('Error getting timezone:', err);
+async function main() {
+    // Get the timezone
+    const timezone = await getCalendarTimezone();
 
-    const timezone = res.data.value;
-    console.log(timezone);
+    const calendars = await calendar.calendarList.list(); // Get all calendars
 
-    const calendar_ids = await calendar.calendarList.list();
-    const calendar_id = calendar_ids.data.items.find(x => x.summary == process.env.BLOCK_CALENDAR_SUMMARY).id;
+    const selectionCalId = calendars.data.items.find(x => x.summary == process.env.SELECTION_CALENDAR_SUMMARY).id; // Get selection calendar id
+    const eventCalId = calendars.data.items.find(x => x.summary == process.env.EVENT_CALENDAR_SUMMARY).id; // Get event calendar id
 
-    // Get start and end of curretn year
-    const start = DateTime.now().setZone(timezone);
-    const end = DateTime.now().setZone(timezone).endOf('year');
-
-    // Get Events
-    calendar.events.list({
-        calendarId: calendar_id,
-        timeMin: start.toISO(),
-        timeMax: end.toISO(),
+    const selectionCalEvents = await getEvents({
+        calendarId: selectionCalId,
+        timeMin: DateTime.now().setZone(timezone).toISO(),
+        timeMax: DateTime.now().setZone(timezone).endOf('year').toISO(),
         singleEvents: true,
         orderBy: 'startTime'
-    }, (err, res) => {
-
-        if (err) return console.log('The API returned an error: ' + err);
-
-        const events = res.data.items;
-
-        if (events.length) {
-            console.log('Upcoming 10 events:');
-            events.map((event, i) => {
-                const start = event.start.dateTime || event.start.date;
-                console.log(`${start} - ${event.summary}`);
-            });
-        } else {
-            console.log('No upcoming events found.');
-        }
     });
-});
+
+    if (selectionCalEvents.length === 0) {
+        console.log('No selection events found.');
+        return;
+    }
+
+    // iterate through selection events
+    for (const selectionEvent of selectionCalEvents) {
+        const start = selectionEvent.start.dateTime || selectionEvent.start.date;
+        const end = selectionEvent.end.dateTime || selectionEvent.end.date;
+
+        // Get events in time range of selection event
+        const eventCalEvents = await getEvents({
+            calendarId: eventCalId,
+            timeMin: new Date(start).toISOString(),
+            timeMax:new Date(end).toISOString(),
+            singleEvents: true,
+            orderBy: 'startTime'
+        });
+
+        // Events found
+        console.log(`Found ${eventCalEvents.length} events in ${selectionEvent.summary}`);
+
+        // delete eventCalEvents
+        for (const eventToDel of eventCalEvents) {
+            const result = await deleteEvent(eventToDel, eventCalId);
+        }
+    }
+}
+
+
+function getEvents(params = {}) {
+    return new Promise((resolve, reject) => {
+        calendar.events.list(params, (err, res) => {
+            if (err) return reject('The API returned an error: ' + err);
+
+            const events = res.data.items;
+
+            if (events.length === 0) {
+                //console.log('No events found.');
+            }
+
+            resolve(events);
+        });
+    });
+}
+
+function getCalendarTimezone() {
+    return new Promise((resolve, reject) => {
+        calendar.settings.get({ setting: 'timezone' }, (err, res) => {
+            if (err) return reject('The API returned an error: ' + err);
+
+            const timezone = res.data.value;
+
+            resolve(timezone);
+        });
+    });
+}
+
+function deleteEvent(event, calendarId) {
+    return new Promise((resolve, reject) => {
+        calendar.events.delete({ calendarId: calendarId, eventId: event.id }, (err, res) => {
+            if (err) return reject('The API returned an error: ' + err);
+            
+            console.log(`Deleted event ${event.summary}`);
+            
+            resolve(res);
+        });
+    });
+}
